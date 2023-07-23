df4bfc19e2a2584ca53cd153916022645190f269
Reinhard Sutterluety <info@web-handwerk.com>
2023-07-23 09:27:24 +0200

    Initial commit

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <section className='p-24'>
+      <div className='container'>
+        <h1 className='mt-4 text-3xl font-bold'>NextJS 13 Starter kit</h1>
+      </div>
+    </section>
+  )
+}
